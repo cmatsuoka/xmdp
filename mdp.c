e45538c8be5b11302155fd8009fe449aed0acbb7
Claudio Matsuoka <cmatsuoka@gmail.com>
2013-06-19 07:56:02 -0300

    Relicensed as public domain
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- mdp.c
+++ mdp.c
@@ -1,4 +1,4 @@
-/* xmdp Copyright (C) 1997-2013 Claudio Matsuoka and Hipolito Carraro Jr
+/* xmdp written by Claudio Matsuoka and Hipolito Carraro Jr
  * Original MDP.EXE for DOS by Future Crew
  *
  * 1.4.0: Ported to libxmp 4.1 with sdl sound
@@ -13,23 +13,12 @@
  */
 
 /*
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
+ * This is free and unencumbered software released into the public domain.
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
  */
 
 #include <stdio.h>
